56ef76314794ee12f8fee050eb6be4133f683036
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-11-27 22:00:34 -0800

    changed var for computer letter in line 14

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,14 @@ var wins = 0;
 var losses = 0;
 var numberGuesses = 10;
 var emptyString = "";
-  // have the computer generate a letter
-  var alphabet = "abcdefghijklmnopqrstuvwxyz";
-  while (emptyString.length < 1) {
-      emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+
+// computer's options 
+/*
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+while (emptyString.length < 1) {
+    emptyString += alphabet[Math.floor(Math.random() * alphabet.length)]; 
+    */
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 //==================================================================================================================
 // FUNCTION OF THE GAME
 
@@ -15,42 +19,42 @@ var emptyString = "";
 // if guess matches then add wins
 function updateWins() {
     document.getElementById("#wins").innerHTML = "Wins: " + wins;
- };
+};
 // if guess different then add losses
 function updateLosses() {
     document.getElementById("#losses").innerHTML = "Losses: " + losses;
- };
+};
 // game over if numberGuesses = 0
 function updateGuesses() {
-    document.getElementById("#numberGuesses").innerHTML = "Guesses Left: " + numberGuesses;
- };
+    document.getElementById("numberGuesses").innerHTML = "Guesses Left: " + numberGuesses;
+};
 
- //computer's letter
- function computer() {
-    document.getElementById("#computer").innerHTML = "Computer's Letter: " + numberGuesses;
- };
+//computer's letter
+function computer() {
+    document.getElementById("computer").innerHTML = "Computer's Letter: " + numberGuesses;
+};
 
- //user's pick
- function history() {
-    document.getElementById("#history").innerHTML = "Letter: " + userInput;
- };
+//user's pick
+function history() {
+    document.getElementById("history").innerHTML = "Letter: " + userInput;
+};
 
 
 //==================================================================================================================
 // PROCESS
 // user types a letter
-document.onkeyup = function (event) {
+document.onkeyup = function (userInput) {
     // which key user typed
     var userInput = event.key;
-  
+    //computer picks a letter
+    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     // compare the two letters
-    if (userInput === alphabet) {
+    if (userInput === computerGuess) {
         wins++;
     } else {
         losses++;
         numberGuesses--;
-    }
-}
+    } return userInput;
 };
 
 console.log(userInput);
