0fc42a3a2b9801c54b41e6a4b266d25797bcad35
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-11-27 20:54:53 -0800

    fixed target

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,43 +4,55 @@ var wins = 0;
 var losses = 0;
 var numberGuesses = 10;
 var emptyString = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-
+  // have the computer generate a letter
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  while (emptyString.length < 1) {
+      emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
 //==================================================================================================================
 // FUNCTION OF THE GAME
-// have the computer generate a letter
-while (emptyString.length < 1) {
-    emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
-  } 
-  console.log(emptyString);
+
 
 // if guess matches then add wins
 function updateWins() {
-    document.querySelector("#wins").innerHTML = "Wins: " + wins;
-  }
+    document.getElementById("#wins").innerHTML = "Wins: " + wins;
+ };
 // if guess different then add losses
-function updateLoses() {
-document.querySelector("#losses").innerHTML = "Losses: " + losses;
-}
+function updateLosses() {
+    document.getElementById("#losses").innerHTML = "Losses: " + losses;
+ };
 // game over if numberGuesses = 0
-function updatenumberGuesses() {
-    document.querySelector("#numberGuesses").innerHTML = "Number of guesses left: " + numberGuesses;
-    }
+function updateGuesses() {
+    document.getElementById("#numberGuesses").innerHTML = "Guesses Left: " + numberGuesses;
+ };
+
+ //computer's letter
+ function computer() {
+    document.getElementById("#computer").innerHTML = "Computer's Letter: " + numberGuesses;
+ };
+
+ //user's pick
+ function history() {
+    document.getElementById("#history").innerHTML = "Letter: " + userInput;
+ };
+
+
 //==================================================================================================================
 // PROCESS
 // user types a letter
-document.onkeyup = function(event) {
-// which key user typed
-var userInput = event.onkeydown();
-document.getElementById('past guesses').innerHTML = "Past Guesses: " + userInput;
-
-// compare the two letters
-if (userInput === alphabet) {
-    wins++;
-    updateWins();
-} else {
-    losses++;
-    updateLoses();
-    numberGuesses--;
+document.onkeyup = function (event) {
+    // which key user typed
+    var userInput = event.key;
+  
+    // compare the two letters
+    if (userInput === alphabet) {
+        wins++;
+    } else {
+        losses++;
+        numberGuesses--;
+    }
 }
 };
+
+console.log(userInput);
+console.log(alphabet);
+
